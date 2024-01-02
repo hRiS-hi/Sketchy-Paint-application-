27d549866c12d9a27e236f3e9b3f689d0cc20643
sonamgupta01 <sonam98290@gmail.com>
2024-01-02 16:28:59 +0545

    created root window

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-print("hi")
-print("hi from sonam")
+from tkinter import *
+root = Tk()
+root.title("Paint application")
+root.geometry("1100x600")
 
-print("from sonam branch")
+frame1 = Frame(root, height=100, width=1100, bg="white", highlightthickness=0.5, highlightbackground="white")
+frame1.grid(row=0,column=0)
+
+frame2 = Frame(root, height=500, width=1100, bg="white", highlightthickness=0.5, highlightbackground="white")
+frame2.grid(row=1,column=0)
+
+canvas = Canvas(frame2,height=500,width=1100,bg="white")
+canvas.grid(row=1,column=0)
+
+
+root.resizable(False,False)   
+root.mainloop()
